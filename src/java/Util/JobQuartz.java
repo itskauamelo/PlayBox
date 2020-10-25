e3a8877868de979a1d931cd8e4aa92685eb0bc18
Kauã Morateli <kmelo249@gmail.com>
2020-10-25 11:46:03 -0300

    Implantação do Job em Quartz

--- src/java/Util/JobQuartz.java
+++ src/java/Util/JobQuartz.java
@@ -9,19 +9,55 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import DAO.CompraDAO;
 import Model.Compra;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.quartz.CronScheduleBuilder;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
 import org.quartz.SimpleScheduleBuilder;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
 
 /**
  *
  * @author Kaua.Morateli
  */
+
+
+
+public class JobQuartz {
+    
+    public static void main(String[] args) {
+        SchedulerFactory shedFact = new StdSchedulerFactory();
+        try {
+            Scheduler scheduler = shedFact.getScheduler();
+            scheduler.start();
+            
+            JobDetail job = JobBuilder.newJob(ValidadorJob.class)
+                            .withIdentity("validadorJOB", "grupo01")
+                            .build();
+            
+            Trigger trigger = TriggerBuilder.newTrigger()
+                              .withIdentity("validadorTRIGGER", "grupo01")
+                              .withSchedule(CronScheduleBuilder.cronSchedule("0 0 0 * * ?"))
+                              .build();
+            
+            scheduler.scheduleJob(job, trigger);
+            
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
+
+/*
 public class JobQuartz implements Job {
 
 JobDetail jobDetail = JobBuilder.newJob(JobQuartz.class)
@@ -36,20 +72,19 @@ Trigger trigger = TriggerBuilder.newTrigger()
 				.withIdentity("trigger", "grupo")
 				.withSchedule(simpleScheduler)
 				.build();
-
-
     
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         CompraDAO dao = new CompraDAO();
-        Compra compra = new Compra();
-        try {
-            dao.cancelarPedido(compra);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(JobQuartz.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(JobQuartz.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    try {
+        System.out.println("Validando dados para execução da rotina");
+        dao.cancelarPedido();
+        System.out.println("Rotina executada");
+    } catch (ClassNotFoundException ex) {
+        Logger.getLogger(JobQuartz.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (SQLException ex) {
+        Logger.getLogger(JobQuartz.class.getName()).log(Level.SEVERE, null, ex);
     }
-}
+        }
+    }*/
     
---
+++ src/java/Util/ValidadorJob.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+import java.util.Date;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import DAO.CompraDAO;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Kaua.Morateli
+ */
+public class ValidadorJob implements Job {
+    
+    @Override
+    public void execute (JobExecutionContext arg0) throws JobExecutionException {
+        
+        System.out.println("Iniciando validação dos dados no banco. Iniciado em: "+new Date());
+        CompraDAO dao = new CompraDAO();
+        try {
+            dao.cancelarPedido();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ValidadorJob.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ValidadorJob.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Job executado em "+new Date());
+        
+        
+        
+    }
+    
+}
